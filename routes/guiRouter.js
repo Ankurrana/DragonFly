424c42bcb9251bb6ab89eae3bfd16745342646db
Ankur rana <ankurofficial@hotmail.com>
2016-04-23 15:23:39 +0530

    Removed Unnessasary files

--- routes/guiRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var guiController = require('../controllers/guiController.js');
-var auth = require('../middlewares/auth.js');
-var passport  = require('passport');
-
-router.get('/',guiController.Get_index);
-router.get('/mytasks',passport.authenticate('jwt',{session:false}),guiController.Get_myTasks);
-module.exports = router;
